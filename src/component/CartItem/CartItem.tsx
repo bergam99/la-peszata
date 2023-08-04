4223962787918c036bec6ab14618eee1a423abb1
Jiyun <grosjeanjiyun@icloud.com>
2023-08-04 13:22:54 +0200

    css

--- src/component/CartItem/CartItem.tsx
+++ src/component/CartItem/CartItem.tsx
@@ -4,6 +4,12 @@ import { useCartContext } from "../../context/ShoppingCartContext";
 import formatCurrency from "../../utilities/formatCurrency";
 import QuantityPicker from "../QuantityPicker/QuantityPicker";
 import style from "./CartItem.module.css";
+import { NavLink } from "react-router-dom";
+import { GrFormAdd } from "react-icons/gr";
+import { FaCcPaypal, FaCcVisa } from "react-icons/fa";
+import { SiApplepay } from "react-icons/si";
+import ReusableButtonOne from "../ReusableButtonOne/ReusableButtonOne";
+
 const CartItem = () => {
   const { products, resetCart, removeProduct, removeOne, addOne } =
     useCartContext();
@@ -45,6 +51,27 @@ const CartItem = () => {
           </li>
         ))}
       </ul>
+      <div className={style.addProduct}>
+        <NavLink to="/products">
+          <button className={style.button}>
+            <p> add more products</p>
+            <p>
+              <GrFormAdd />
+            </p>
+          </button>
+        </NavLink>
+      </div>
+
+      <div className={style.pay}>
+        Payment Method{" "}
+        <div>
+          <FaCcVisa /> <SiApplepay /> <FaCcPaypal />
+        </div>
+      </div>
+      <ReusableButtonOne
+        title="continue"
+        callback={() => console.log("acceder au paiement")}
+      />
       <button onClick={reset}>Reset</button>
     </main>
   );
