8aaae88da419482b27eeecb3ece62b60e0b6ce95
Thomas HEROGUELLE <totosh93270@gmail.com>
2023-08-28 12:14:02 +0200

    done

--- src/component/FilteredCategoryProducts/FilteredCategoryProducts.tsx
+++ src/component/FilteredCategoryProducts/FilteredCategoryProducts.tsx
@@ -42,148 +42,151 @@ const FilteredCategoryProducts = ({ products }: { products: IProduct[] }) => {
     setSelectedCategory(undefined);
   };
 
-  return (
-    <div>
-      <div className={style.displayFlex}>
-        <ReusableButtonOne
-          title="Pizzas"
-          callback={() => setSelectedCategory("pizza")}
+ 
 
-        />
-        <ReusableButtonOne
-          title="Burgers"
-          callback={() => setSelectedCategory("burger")}
-
-        />
-        <ReusableButtonOne
-          title="Desserts"
-          callback={() => setSelectedCategory("dessert")}
-
-        />
-        <ReusableButtonOne
-          title="Boissons"
-          callback={() => setSelectedCategory("drink")}
-
-        />
-        <ReusableButtonOne
-          title="Tous les produits" callback={showAllProducts} />
-      </div>
-
-      <div className={style.displayFlex}>
-        <ReusableButtonOne
-          title="Vegan"
-          callback={() => handleTagFilter("Vegan")}
-        />
-        <ReusableButtonOne
-          title="No porc"
-          callback={() => handleTagFilter("No porc")}
-        />
-        <ReusableButtonOne
-          title="No sugar"
-          callback={() => handleTagFilter("No sugar")}
-
-
-        />
-        <ReusableButtonOne
-          title="Fat food"
-          callback={() => handleTagFilter("Fat food")}
-
-        />
-        <ReusableButtonOne
-          title="Sodas"
-          callback={() => handleTagFilter("Sodas")}
-
-        />
-        <ReusableButtonOne
-          title="Healthy"
-          callback={() => handleTagFilter("Healthy")}
-
-        />
-      </div>
-
-      <div className={style.wrapperMobile}>
-        <div className={style.mobile}>
+    return (
+      <div>
+        <div className={style.displayFlex}>
           <ReusableButtonOne
             title="Pizzas"
             callback={() => setSelectedCategory("pizza")}
-            icon={<img className={style.pizzaIcon} src={pizzaIcon} alt="Pizza Icon" />}
+
+
           />
           <ReusableButtonOne
             title="Burgers"
             callback={() => setSelectedCategory("burger")}
-            icon={<img className={style.burgerIcon} src={burgerIcon} alt="Pizza Icon" />}
 
           />
           <ReusableButtonOne
             title="Desserts"
             callback={() => setSelectedCategory("dessert")}
-            icon={<img className={style.cupcakeIcon} src={cupcakeIcon} alt="Pizza Icon" />}
-            
 
           />
           <ReusableButtonOne
             title="Boissons"
             callback={() => setSelectedCategory("drink")}
-            icon={<img className={style.drinkIcon} src={drinkIcon} alt="Pizza Icon" />}
 
           />
           <ReusableButtonOne
-            title="Tous les produits" callback={showAllProducts}
-            icon={<img className={style.fastFoodIcon} src={fastFoodIcon} alt="Pizza Icon" />}
-          />
+            title="Tous les produits" callback={showAllProducts} />
         </div>
-        <div className={style.mobile}>
+
+        <div className={style.displayFlex}>
           <ReusableButtonOne
             title="Vegan"
             callback={() => handleTagFilter("Vegan")}
-            icon={<img className={style.veganIcon} src={veganIcon} alt="Pizza Icon" />}
-
           />
           <ReusableButtonOne
             title="No porc"
             callback={() => handleTagFilter("No porc")}
-            icon={<img className={style.noPocrIcon} src={noPocrIcon} alt="Pizza Icon" />}
-
           />
           <ReusableButtonOne
             title="No sugar"
             callback={() => handleTagFilter("No sugar")}
-            icon={<img className={style.sugarFreeIcon} src={sugarFreeIcon} alt="Pizza Icon" />}
+
 
           />
           <ReusableButtonOne
             title="Fat food"
             callback={() => handleTagFilter("Fat food")}
-            icon={<img className={style.fatIcon} src={fatIcon} alt="Pizza Icon" />}
 
           />
           <ReusableButtonOne
             title="Sodas"
             callback={() => handleTagFilter("Sodas")}
-            icon={<img className={style.sodaIcon} src={sodaIcon} alt="Pizza Icon" />}
+
           />
           <ReusableButtonOne
             title="Healthy"
             callback={() => handleTagFilter("Healthy")}
-            icon={<img className={style.healthIcon} src={healthIcon} alt="Pizza Icon" />}
 
           />
         </div>
-      </div>
+
+        <div className={style.wrapperMobile}>
+          <div className={style.mobile}>
+            <ReusableButtonOne
+              title="Pizzas"
+              callback={() => setSelectedCategory("pizza")}
+              icon={<img className={style.pizzaIcon} src={pizzaIcon} alt="Pizza Icon" />}
+            />
+            <ReusableButtonOne
+              title="Burgers"
+              callback={() => setSelectedCategory("burger")}
+              icon={<img className={style.burgerIcon} src={burgerIcon} alt="Pizza Icon" />}
+
+            />
+            <ReusableButtonOne
+              title="Desserts"
+              callback={() => setSelectedCategory("dessert")}
+              icon={<img className={style.cupcakeIcon} src={cupcakeIcon} alt="Pizza Icon" />}
 
 
-      <div className={style.elementsContainer}>
-        {filteredTagProducts.map((product) => (
-          <div key={product.id}>
-            <Link to={`/products/${product.id}`} className={style.flex}>
-              <ProductCard item={product} showButton={true} />
-            </Link>
+            />
+            <ReusableButtonOne
+              title="Boissons"
+              callback={() => setSelectedCategory("drink")}
+              icon={<img className={style.drinkIcon} src={drinkIcon} alt="Pizza Icon" />}
+
+            />
+            <ReusableButtonOne
+              title="Tous les produits" callback={showAllProducts}
+              icon={<img className={style.fastFoodIcon} src={fastFoodIcon} alt="Pizza Icon" />}
+            />
           </div>
-        ))}
+          <div className={style.mobile}>
+            <ReusableButtonOne
+              title="Vegan"
+              callback={() => handleTagFilter("Vegan")}
+              icon={<img className={style.veganIcon} src={veganIcon} alt="Pizza Icon" />}
+
+            />
+            <ReusableButtonOne
+              title="No porc"
+              callback={() => handleTagFilter("No porc")}
+              icon={<img className={style.noPocrIcon} src={noPocrIcon} alt="Pizza Icon" />}
+
+            />
+            <ReusableButtonOne
+              title="No sugar"
+              callback={() => handleTagFilter("No sugar")}
+              icon={<img className={style.sugarFreeIcon} src={sugarFreeIcon} alt="Pizza Icon" />}
+
+            />
+            <ReusableButtonOne
+              title="Fat food"
+              callback={() => handleTagFilter("Fat food")}
+              icon={<img className={style.fatIcon} src={fatIcon} alt="Pizza Icon" />}
+
+            />
+            <ReusableButtonOne
+              title="Sodas"
+              callback={() => handleTagFilter("Sodas")}
+              icon={<img className={style.sodaIcon} src={sodaIcon} alt="Pizza Icon" />}
+            />
+            <ReusableButtonOne
+              title="Healthy"
+              callback={() => handleTagFilter("Healthy")}
+              icon={<img className={style.healthIcon} src={healthIcon} alt="Pizza Icon" />}
+
+            />
+          </div>
+        </div>
+
+
+        <div className={style.elementsContainer}>
+          {filteredTagProducts.map((product) => (
+            <div key={product.id}>
+              <Link to={`/products/${product.id}`} className={style.flex}>
+                <ProductCard item={product} showButton={true} />
+              </Link>
+            </div>
+          ))}
+        </div>
+
       </div>
+    );
+  };
 
-    </div>
-  );
-};
-
-export default FilteredCategoryProducts;
+  export default FilteredCategoryProducts;
